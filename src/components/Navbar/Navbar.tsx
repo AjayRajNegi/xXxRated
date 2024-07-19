b4cef36e0b827c23525d9d405e7e1c4b8844c563
Ajay <ajayrajnegi111@gmail.com>
2024-07-19 23:46:28 +0530

    logo change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,10 +4,10 @@ import NeuButton from "./NeuButton/NeuButton";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 z-30 hidden h-max w-full items-center justify-between px-4 pt-2 md:flex md:px-8">
+    <nav className="fixed top-0 z-30 hidden h-max w-full items-center justify-between px-4 pt-4 md:flex md:px-8">
       <div className="flex items-center space-x-2">
         <h1 className="text-xl font-bold text-white md:text-2xl">XXXRATED</h1>
-        <img src="./blacklogo .png" alt="logo image" className="h-20 w-20" />
+        <img src="./wlogo.svg" alt="logo image" className="h-[3rem] w-[3rem]" />
       </div>
       {/* Navbar for large devices. */}
       <ul
