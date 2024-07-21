3e042defff8b4efda24b51fef8631de2312a7fda
Ajay <ajayrajnegi111@gmail.com>
2024-07-21 23:54:05 +0530

    rarted tracking

--- src/components/CardPrallax/CardParallax.jsx
+++ src/components/CardPrallax/CardParallax.jsx
@@ -5,7 +5,7 @@ import Card from "./Card";
 export default function CardParallax() {
   return (
     <>
-      <p className="bg-white pb-[4vw] pl-[4vw] pt-[4vw] text-2xl uppercase tracking-[-5px] text-black md:text-4xl lg:text-7xl">
+      <p className="bg-white pb-[4vw] pl-[4vw] pt-[4vw] text-2xl uppercase tracking-[-2px] text-black md:text-4xl md:tracking-[-5px] lg:text-7xl">
         why choose <br /> xxxrated
       </p>
       <main className={styles.main}>
